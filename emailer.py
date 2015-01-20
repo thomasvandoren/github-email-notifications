ba5e0f1912682c6fa6273eebb1d0801f4f01f5a7
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-19 18:37:57 -0800

    redirect to chapel-lang.org on index page

--- emailer.py
+++ emailer.py
@@ -1,8 +1,17 @@
 import os
 from flask import Flask
+import flask
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
+heroku = Heroku(app)
 
 @app.route('/')
 def index():
-    return 'Move along, nothing to see here.'
+    """Redirect to chapel homepage."""
+    return flask.redirect('http://chapel-lang.org/', code=301)
+
+@app.route('/commit-email', methods=['POST'])
+def commit_email():
+    """Receive web hook from github and generate email."""
+    return 'blah'
