0c2bf84777db7c4b94a783111f2e18172b110ac9
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-19 20:29:46 -0800

    add todo, update msg format, and ensure short subject

--- emailer.py
+++ emailer.py
@@ -33,6 +33,17 @@ def index():
 @app.route('/commit-email', methods=['POST'])
 def commit_email():
     """Receive web hook from github and generate email."""
+
+    # TODO: look at X-GitHub-Event value (thomasvandoren, 2015-01-19)
+    # TODO: add secret to webhook and set secret in environment (thomasvandoren, 2015-01-19)
+    # TODO: then, look at X-GitHub-Signature and verify body. See:
+    #
+    #         https://developer.github.com/webhooks/#payloads
+    #         https://developer.github.com/v3/repos/hooks/#create-a-hook
+    #         https://github.com/github/github-services/blob/f3bb3dd7/lib/service/http_helper.rb#L77
+    #
+    #       (thomasvandoren, 2015-01-19)
+
     json_dict = flask.request.get_json()
     logging.info('json body: {0}'.format(json_dict))
 
@@ -50,7 +61,7 @@ def commit_email():
 
     msg_info = {
         'repo': json_dict['repository']['full_name'],
-        'revision': json_dict['head_commit']['url'],
+        'revision': json_dict['head_commit']['id'],
         'message': json_dict['head_commit']['message'],
         'changed_files': changes,
         'pusher': json_dict['pusher']['name'],
@@ -60,12 +71,12 @@ def commit_email():
     sender = os.environ.get('CHAPEL_EMAILER_SENDER')
     recipient = os.environ.get('CHAPEL_EMAILER_RECIPIENT')
 
-    subject = '[{0}] {1}'.format(
-        msg_info['repo'],
-        msg_info['message'].splitlines()[0])
+    subject_msg = msg_info['message'].splitlines()[0]
+    subject_msg = subject_msg[:50]
+    subject = '[{0}] {1}'.format(msg_info['repo'], subject_msg)
+
     body = """Revision: {revision}
 Author: {pusher}
-Compare: {compare_url}
 Log Message:
 ------------
 {message}
@@ -73,6 +84,8 @@ Log Message:
 Modified Files:
 ---------------
 {changed_files}
+
+Compare: {compare_url}
 """.format(**msg_info)
 
     msg = envelopes.Envelope(
